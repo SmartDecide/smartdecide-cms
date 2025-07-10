efcabc84546833b2c978fb60eaa182e94b97bc15
bk <bk@tangibleitdemo.com>
2025-07-10 15:59:58 +0530

    Updated content type of blog and connect with azure

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,59 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com", // Required for Strapi < 4.10.6; you can remove it otherwise
+            "https://market-assets.strapi.io", // Required for Strapi >= 4.10.6; you can remove it otherwise
+            /**
+             * Note: If using a STORAGE_URL, replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` with `process.env.STORAGE_URL`.
+             * If using a CDN URL, make sure to include that URL in the CSP headers, e.g., `process.env.STORAGE_CDN_URL`.
+             */
+            `https://${process.env.AZURE_STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com", // Required for Strapi < 4.10.6; you can remove it otherwise
+            /**
+             * Note: If using a STORAGE_URL, replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` with `process.env.STORAGE_URL`.
+             * If using a CDN URL, make sure to include that URL in the CSP headers, e.g., `process.env.STORAGE_CDN_URL`.
+             */
+            `https://${process.env.AZURE_STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+// module.exports = () => ({});
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-sa",
+      providerOptions: {
+        account: env("AZURE_STORAGE_ACCOUNT"),
+        accountKey: env("AZURE_STORAGE_ACCOUNT_KEY"),
+        containerName: env("AZURE_STORAGE_CONTAINER_NAME"),
+        defaultPath: env("AZURE_BLOG_MEDIA_PATH", "uploads"),
+      },
+    },
+  },
+});
